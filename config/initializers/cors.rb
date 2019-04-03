52763550c1e6855c9f390be26dd8faf8a7f5838b
rob <robertchen234@gmail.com>
2019-04-03 09:25:48 -0400

    cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://robertchen.dev'
+    origins 'https://robertchen.dev, https://robertchen.netlify.com'
 
     resource '*',
       headers: :any,
