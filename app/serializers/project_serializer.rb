d0080256f8b1e05892e11f6c0a9c31f6554bc39d
rob <robertchen234@gmail.com>
2019-04-15 18:05:29 -0400

    disable projects info

--- app/serializers/project_serializer.rb
+++ app/serializers/project_serializer.rb
@@ -1,3 +1,5 @@
 class ProjectSerializer < ActiveModel::Serializer
-  attributes :id, :title, :teaser, :description, :year, :tech_stack, :demo_url, :git_url, :video_url, :image_url, :claps
+  attributes :id, :claps
 end
+
+# :title, :teaser, :description, :year, :tech_stack, :demo_url, :git_url, :video_url, :image_url, 
